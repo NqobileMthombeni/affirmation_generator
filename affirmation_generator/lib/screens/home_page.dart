ca91a85b9579be18059aa4fee8d7c5fe53be4990
Karabosithole <sitholekarabo0@gmail.com>
2024-08-25 09:25:57 +0200

    Added more afirmations

--- affirmation_generator/lib/screens/home_page.dart
+++ affirmation_generator/lib/screens/home_page.dart
@@ -12,7 +12,18 @@ class _MyHomePageState extends State<MyHomePage> {
     "Believe in yourself and all that you are.",
     "Every day is a new beginning.",
     "You have the power to create change.",
-    "Embrace the journey and enjoy the ride."
+    "Embrace the journey and enjoy the ride.",
+    "Everyday is a new beginning",
+    "Perseverance flows through my veins, propelling me forward with unwavering resolve.",
+    "I believe in my ability to persevere and create the life I desire.",
+    "Each setback is a stepping stone on my path to greatness.",
+    "I possess the inner strength and courage to persevere in the face of uncertainty.",
+    "I am steadfast in my commitment to achieving my goals.",
+    "Perseverance fuels my progress and propels me towards success.",
+    "I am capable of persevering through any adversity that comes my way.",
+    "I embrace obstacles as opportunities to prove my perseverance.",
+    "With each challenge I face, my determination grows stronger.",
+    "I am a resilient soul, and perseverance is my guiding force."
   ];
 
   String _currentAffirmation = "Click the button to get an affirmation!";
